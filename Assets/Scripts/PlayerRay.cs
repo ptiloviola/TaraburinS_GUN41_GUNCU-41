d854b87b856bc298d44cc298cc823c7030ce31db
ptiloviola <ptiloviola@gmail.com>
2026-06-17 17:47:14 +0300

    another commit

--- Assets/Scripts/PlayerRay.cs
+++ Assets/Scripts/PlayerRay.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class PlayerRay : MonoBehaviour
 {
+    [SerializeField] private float _size = 10f;
     private void Update()
     {
-        
-    }
+        Ray ray = new Ray(transform.position, transform.forward);
+        // ray.origin = Vector3.left;
+        // ray.direction = Vector3.up;
 
+        // Debug.DrawRay(transform.position, transform.forward * _size, Color.red);
+        Debug.DrawRay(ray.origin, ray.direction * _size, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log($"Hit {hit.collider.gameObject.name}");
+        }
+    }
 }
