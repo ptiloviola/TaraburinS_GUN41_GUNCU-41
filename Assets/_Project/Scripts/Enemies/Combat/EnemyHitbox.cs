9ede5d4c64b2937905bbe4f9fca21bd8830e0205
ptiloviola <ptiloviola@gmail.com>
2026-08-14 00:23:06 +0300

    added enemy hitbox

---
+++ Assets/_Project/Scripts/Enemies/Combat/EnemyHitbox.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TpsShooter.Combat;
+using TpsShooter.Enemies.Core;
+
+namespace TpsShooter.Enemies.Combat
+{
+    public class EnemyHitbox : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private EnemyBrain _brain;
+        
+        [Tooltip("Множитель урона. Для головы ставь 2.0, для тела 1.0")]
+        [SerializeField] private float _damageMultiplier = 1.0f;
+        
+        [Tooltip("Опционально: Имя части тела для дебага (Head, Torso, Limbs)")]
+        [SerializeField] private string _bodyPartName = "Body";
+
+        public void TakeDamage(float amount)
+        {
+            if (_brain == null || _brain.Health.IsDead) return;
+
+            float finalDamage = amount * _damageMultiplier;
+            
+            Debug.Log($"<color=orange>[Hitbox]</color> Попадание в {_bodyPartName}! Урон: {amount} x {_damageMultiplier} = {finalDamage}");
+            
+            // Передаем умноженный урон в мозг
+            _brain.TakeDamage(finalDamage);
+        }
+    }
+}
